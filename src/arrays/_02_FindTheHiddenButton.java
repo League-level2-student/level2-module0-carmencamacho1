f2af2f2466200b005ba5d281cc21851f324a19d3
Carmen C <Carmen.Camacho@gompersstudent.org>
2018-10-08 14:27:53 -0700

    FindTheHiddenButton

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -19,10 +19,11 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JFrame window;
 	JPanel panel;
 	
+	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] button;
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
@@ -33,26 +34,34 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		      String enter = JOptionPane.showInputDialog("Enter a positive number");
+		      int numberEntered = Integer.parseInt(enter);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+	 button = new JButton[numberEntered];
 		//5. Make a for loop to iterate through the JButton array
+	 for(int i = 0; i < button.length; i++ ){
+		 button[i] =  new JButton();
+		 button[i].addActionListener(this);
+		 panel.add(button[i]);
+		 
+	 }
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
 		
 		//9 add the panel to the window
-		
+		window.add(panel);
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		//11. set the JFrame to visible.
-		
+		window.setVisible(true);
 		//12. Give the user the instructions for the game.
-		
+		JOptionPane.showMessageDialog(null, "Your job: click the hidden button ");
 		//13. initialize the hiddenButton variable to a random number less than the int created int step 3
-		
+		Random randNum = new Random();
+		hiddenButton = randNum.nextInt(button.length);
 		//14. Set the text of the JButton located at hiddenButton the read "ME"
-
+		
 		//15. Use Thread.sleep(100); to pause the program.
 		//    Surround it with a try/catch
 		
